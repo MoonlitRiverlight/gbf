e31dfa49f63c0f43159466b6ca371914a1cebd76
MoonlitRiverlight <100371672+MoonlitRiverlight@users.noreply.github.com>
2022-02-24 16:39:42 -0300

    Add files via upload

--- build-pages.sh
+++ build-pages.sh
@@ -5,6 +5,6 @@ cp index.html public/index.html
 sed -i "s/#raid/$(cat dist/raid.js)/" public/index.html
 sed -i "s/#character/$(cat dist/character.js)/" public/index.html
 sed -i "s/#weapon/$(cat dist/weapon.js)/" public/index.html
-sed -i "s/#weapon jp/$(cat dist/weaponjp.js)/" public/index.html
+sed -i "s/#weapon jp/$(cat dist/weapon jp.js)/" public/index.html
 sed -i "s/#summon/$(cat dist/summon.js)/" public/index.html
 sed -i "s/#date/$(date)/" public/index.html
